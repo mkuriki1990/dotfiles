7763cbe0a8593c73958787c1ae6a3b3d410ffb9e
mkuriki1990 <mkuriki1990@gmail.com>
2018-03-24 12:06:49 +0900

    modify symbolic link target

--- mklink.sh
+++ mklink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -sf ~/.dotfiles/.vimrc
-ln -sf ~/.dotfiles/.zshrc
-ln -sf ~/.dotfiles/.tmux.conf
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
