8c51523d9a7a74cd20b7a435dde347e4db491d58
mkuriki1990 <mkuriki1990@gmail.com>
2019-01-14 16:05:50 +0900

    add comments and tips

--- mklink.sh
+++ mklink.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# 各設定ファイルのシンボリックリンクを作成するスクリプト
+# その他メモなど
 
 ln -sf ~/.dotfiles/zshenv ~/.zshenv
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+
+# 自動で設定ファイルを更新するためには以下のコマンド列を
+# crontab に登録すればよい (時間は任意)
+# 28 6 * * * cd ${HOME}/.dotfiles/ && git pull && ${HOME}/.dotfiles/mklink.sh
